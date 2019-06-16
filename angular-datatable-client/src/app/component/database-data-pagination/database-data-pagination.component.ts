0f08cb1370f78702feebf8fbf6aed719a257aa7c
bms241093 <balmukund.singh241@gmail.com>
2019-06-16 14:16:28 +0530

    filters have been added on UI-Grid

--- angular-datatable-client/src/app/component/database-data-pagination/database-data-pagination.component.ts
+++ angular-datatable-client/src/app/component/database-data-pagination/database-data-pagination.component.ts
@@ -14,38 +14,90 @@ export class DatabaseDataPaginationComponent implements OnInit {
   page = new Page();
   rows = new Array<CityModel>();
 
-  allEmployee:any;
+  allEmployee: any;
+  temp = [];
 
-  constructor(private paginationService: PaginationService,private http:HttpDataService) {
+  constructor(private paginationService: PaginationService, private http: HttpDataService) {
     this.page.pageNumber = 0;
-    this.page.size = 10; 
+    this.page.size = 10;
   }
 
-  ngOnInit()
-   {
-    this.http.post('findall',{}).subscribe(
-     (res)=>{
-       this.allEmployee = res.totalCity;
-       this.setPage({ offset: 0 });
-    },
-    (err)=>{},
-    ()=>{}
+  ngOnInit() {
+    this.http.post('findall', {}).subscribe(
+      (res) => {
+        this.allEmployee = res.totalCity;
+        this.temp = res.totalCity;
+        this.rows = res.totalCity;
+        this.setPage({ offset: 0 });
+      },
+      (err) => { },
+      () => { }
     );
-    
+
   }
 
   /**
    * Populate the table with new data based on the page number
    * @param page The page to select
    */
-  setPage(pageInfo){
+  setPage(pageInfo) {
     this.page.pageNumber = pageInfo.offset;
-    this.paginationService.getResults(this.page,this.allEmployee).subscribe(pagedData => {
+    this.paginationService.getResults(this.page, this.allEmployee).subscribe(pagedData => {
       this.page = pagedData.page;
       this.rows = pagedData.data;
-      console.log('-----pagedData.data----',pagedData.data);
-      console.log('-----this.rows----',this.rows);
     });
+  }
+    nameFilter(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.temp.filter(function (d) {
+     return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.page.pageNumber = 0;
+  }
+  countryCodeFilter(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.temp.filter(function (d) {
+      return d.countryCode.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.page.pageNumber = 0;
+  }
+  districtFilter(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.temp.filter(function (d) {
+      return d.district.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.page.pageNumber = 0;
+  }
+  populationFilter(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.temp.filter(function (d) {
+      return d.population.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.page.pageNumber = 0;
   }
 
 }
